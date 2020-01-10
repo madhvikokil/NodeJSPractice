b112f53a530e83067fece5bba00208cbe7586475
Madhvi Kokil <madhvi.kokil@cuelogic.com>
2020-01-10 11:42:15 +0530

    assignment 1

--- HTTP/httpRequest.js
+++ HTTP/httpRequest.js
@@ -1,8 +1,13 @@
 let http = require('http');
+const db = require("db");
+require('dotenv').config()
+const port = process.env.DB_PORT ;
+const name = process.env.DB_NAME
+
 
 http.createServer((req,res) => {
     if(req.url === "/"){
-        res.write("Successfully in the port");
+        res.write("Successfully in the port",name);
         res.end();
     }
         
@@ -10,6 +15,9 @@ http.createServer((req,res) => {
         res.write("In api route");
         res.end()
     }
-}).listen(8085);
+}).listen(port);
 
+
+console.log("fggg");
 console.log("HTTP Request");
+console.log("process env :",process.env.PORT);
